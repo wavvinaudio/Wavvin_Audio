54b0719074d2b06bd6a8b54a70bc4680b6005972
wavvinaudio <wavvinaud@gmail.com>
2026-08-28 18:18:26 -0700

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'Wavvin_Audio',
+  base: '/Wavvin_Audio/',
   plugins: [react()],
 })
